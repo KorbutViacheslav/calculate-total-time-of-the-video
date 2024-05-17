bd761f3f0c95f9c398e89e92a8ca99f76c8c89aa
Viacheslav Korbut <minowaman777@gmail.com>
2024-05-17 13:39:39 +0300

    a

--- src/main/java/org/profitsoft/App.java
+++ src/main/java/org/profitsoft/App.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class App {
     public static void main(String[] args) {
         String folderPath = "E:\\JavaScript\\LESSONS\\[Udemy, Bogdan Stashchuk] React - Полный Курс по React и Redux\\";
